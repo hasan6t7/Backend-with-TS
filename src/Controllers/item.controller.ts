7cf5b72c8f030d85507548dc058bbdacbc4c6e62
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-10 23:18:35 +0600

    update item by id

--- src/Controllers/item.controller.ts
+++ src/Controllers/item.controller.ts
@@ -1,5 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
-import { items } from "../Models/item.js";
+import { items, type Item } from "../Models/item.js";
 
 export const postitems = async (
   req: Request,
@@ -28,6 +28,32 @@ export const getitems = async (
 ) => {
   try {
     res.json(items);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const updateItem = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const itemIndex = items.findIndex((item) => item.id === Number(id));
+
+    if (itemIndex === -1) {
+      res.status(404).json({ message: "Not found" });
+      return;
+    }
+
+    const updatedItem: Item = {
+      ...items[itemIndex],
+      name,
+      id: Number(id) 
+    };
+
+    items[itemIndex] = updatedItem;
+    res.status(200).json(updatedItem);
   } catch (error) {
     next(error);
   }
